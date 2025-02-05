a48957d2ec437a84e6042a3a45fb36752d5b16c4
Ford-byte <c.iyac123@gmail.com>
2025-02-05 17:00:38 +0800

    chore:2nd

--- app/_layout/header.jsx
+++ app/_layout/header.jsx
@@ -3,7 +3,7 @@ import Navigation from "./menu/navigation";
 
 export default function Header() {
   return (
-    <header className="relative center bg-gray-200">
+    <header className="fixed z-[1000] center bg-gray-200">
       <div className="w-full h-[100px] flex items-center justify-between container">
         <div>
           <Logo />
--- app/_layout/menu/navigation.jsx
+++ app/_layout/menu/navigation.jsx
@@ -1,3 +1,31 @@
+import Link from "next/link";
+
 export default function Navigation() {
-  return <div>Navigation</div>;
+  const navigators = [
+    {
+      title: "Home",
+      link: "/",
+    },
+    {
+      title: "About",
+      link: "/about",
+    },
+    {
+      title: "Contact",
+      link: "/contact",
+    },
+  ];
+  return (
+    <div id="menu" className="relative">
+      <div className="flex gap-x-[36px]">
+        {navigators?.map((item, index) => {
+          return (
+            <Link href={item?.link} key={index} className="primary-button">
+              {item?.title}
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
