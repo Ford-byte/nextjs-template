2f38f52dba9e45cab726ed2411eced88b77cf208
Ford-byte <c.iyac123@gmail.com>
2025-02-13 14:33:28 +0800

    update

--- app/profile/page.js
+++ app/profile/page.js
@@ -5,16 +5,16 @@ import Error from "./error";
 import ProfileBanner from "@/components/blocks/ProfileBanner";
 import UserDetails from "@/components/blocks/UserDetails";
 
+const Loading = () => {
+  return (
+    <div className="w-full min-h-[700px] flex justify-center items-center bg-black">
+      <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin" />
+    </div>
+  );
+};
+
 export default function Page() {
   const { isLogged, isLoading } = useLocalStorage();
-
-  const Loading = () => {
-    return (
-      <div className="w-full min-h-[700px] center items-center bg-black">
-        <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin" />
-      </div>
-    );
-  };
 
   if (!isLoading) {
     return <Loading />;
@@ -23,11 +23,11 @@ export default function Page() {
   return isLogged ? (
     <div className="min-h-[700px] relative">
       <ProfileBanner
-        name={`Rex`}
-        wallpic={`/images/rex.webp`}
-        profile={`/images/rex.webp`}
+        name="Rex"
+        wallpic="/images/rex.webp"
+        profile="/images/rex.webp"
       />
-      <UserDetails name={`Rex`} />
+      <UserDetails name="Rex" />
     </div>
   ) : (
     <Error />
--- components/blocks/ProfileBanner.jsx
+++ components/blocks/ProfileBanner.jsx
@@ -2,34 +2,50 @@
 import { useState } from "react";
 import Image from "next/image";
 import ImagePopup from "../popups/ImagePopup";
+import Close from "@/public/icons/close";
+import useLocalStorage from "../store/localStorage";
 
 export default function ProfileBanner({ wallpic, profile }) {
   const [selectedImage, setSelectedImage] = useState(null);
-
+  const { isLogged } = useLocalStorage();
   return (
     <div className="relative flex flex-col items-center min-h-[400px] lg:min-h-[450px]">
       {wallpic ? (
-        <Image
-          src={wallpic}
-          width={1920}
-          height={900}
-          alt="Profile Banner"
-          className="w-full h-[300px] object-cover pointer"
-          onClick={() => setSelectedImage(wallpic)}
-        />
+        <div className="w-full h-[300px]">
+          <Image
+            src={wallpic}
+            width={1920}
+            height={900}
+            alt="Profile Banner"
+            className="w-full h-full object-cover pointer"
+            onClick={() => setSelectedImage(wallpic)}
+          />
+          {isLogged && (
+            <div className="absolute -translate-y-1/2 right-4 lg:right-12 bg-gray-100 p-2 rounded-full">
+              <Close className={`size-6 lg:size-12 rotate-45 pointer`} />
+            </div>
+          )}
+        </div>
       ) : (
         <div className="w-full min-h-[300px] lg:min-h-[400px] bg-gray-500 animate-pulse"></div>
       )}
       <div className="absolute flex justify-center w-fit rounded-full translate-y-[100%] lg:translate-y-1/2">
         {profile ? (
-          <Image
-            src={profile}
-            width={300}
-            height={300}
-            alt="Profile Picture"
-            className="size-[200px] lg:size-[300px] rounded-full object-cover cursor-pointer"
-            onClick={() => setSelectedImage(profile)}
-          />
+          <div className="relative">
+            <Image
+              src={profile}
+              width={300}
+              height={300}
+              alt="Profile Picture"
+              className="size-[200px] lg:size-[300px] rounded-full object-cover cursor-pointer"
+              onClick={() => setSelectedImage(profile)}
+            />
+            {isLogged && (
+              <div className="absolute bottom-6 lg:bottom-8 right-0 bg-gray-100 p-2 rounded-full">
+                <Close className={`size-6 lg:size-12 rotate-45 pointer`} />
+              </div>
+            )}
+          </div>
         ) : (
           <div className="bg-black size-[200px] lg:size-[300px] rounded-full" />
         )}
