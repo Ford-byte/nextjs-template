c3640e8863644d359c2a053bd74c5c9587d1d101
Ford-byte <c.iyac123@gmail.com>
2025-02-13 14:02:54 +0800

    rate limit

--- app/axios.js
+++ app/axios.js
@@ -1,19 +1,25 @@
 import axios from "axios";
+import rateLimit from "axios-rate-limit";
 
-const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  timeout: 5000,
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-});
+const apiClient = rateLimit(
+  axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    timeout: 5000,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  }),
+  {
+    maxRequests: 10, // Max number of requests
+    perMilliseconds: 1000, // Time frame (1 second)
+    maxRPS: 10, // Max requests per second
+  }
+);
 
 apiClient.interceptors.response.use(
   (response) => response,
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default apiClient;
