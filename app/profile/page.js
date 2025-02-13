46d7c753f7129f81dfb42a58998d311d932e7ce2
Ford-byte <c.iyac123@gmail.com>
2025-02-13 11:28:04 +0800

    update

--- app/profile/page.js
+++ app/profile/page.js
@@ -10,7 +10,7 @@ export default function Page() {
 
   const Loading = () => {
     return (
-      <div className="w-full min-h-[700px] center items-center">
+      <div className="w-full min-h-[700px] center items-center bg-black">
         <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin" />
       </div>
     );
